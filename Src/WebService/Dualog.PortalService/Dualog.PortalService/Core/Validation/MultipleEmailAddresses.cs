3897ad6f482f744d940b689eb22a7453bc761f63
Lars Espejord <lars.espejord@dualog.com>
2017-09-13 15:23:21 +0200

    Implemented validation of multiple email column (#45)

---
+++ Src/WebService/Dualog.PortalService/Dualog.PortalService/Core/Validation/MultipleEmailAddresses.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dualog.PortalService.Core.Validation
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class MultipleEmailAddresses : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+
+            string text = value as string;
+            // Emails must be separated with ','
+            string[] emails = text.Split(',');
+            foreach (string email in emails)
+            {
+                var trimmedemail = email.Trim();
+                if (!new EmailAddressAttribute().IsValid(trimmedemail)) return new ValidationResult("Invalid email address: " + trimmedemail);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
