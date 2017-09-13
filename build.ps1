ef7fbafb478f2a1f33f3365f8a2a13ef44018f47
Yngve Hals Nyheim <yngve.nyheim@gmail.com>
2017-09-13 11:48:44 +0200

    Adjusted build script exit (5)

--- build.ps1
+++ build.ps1
@@ -191,6 +191,7 @@ try {
         Throw "An error occured while running build script"    
     }
 } catch {
-    Write-Host "##teamcity[message text='BUILD FAILED' status='ERROR']"
-    exit(1)
+    Write-Output-Exception $_.Exception
+    # Write-Host "##teamcity[message text='BUILD FAILED' status='ERROR']"
+    exit 1
 }
