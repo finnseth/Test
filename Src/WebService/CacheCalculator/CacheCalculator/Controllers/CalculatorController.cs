9d64d01510ee064d08811320597cad3b5eddcdbf
Asgeir Finnseth <asgeir@dualog.no>
2017-10-16 13:11:37 +0200

    Dummy version of CacheCalculator works.
    After including a bunch of nugets (with the help of Tor Martin) it now works...

--- Src/WebService/CacheCalculator/CacheCalculator/Controllers/CalculatorController.cs
+++ Src/WebService/CacheCalculator/CacheCalculator/Controllers/CalculatorController.cs
@@ -10,32 +10,56 @@ using System.Net;
 using Microsoft.AspNetCore.Mvc;
 
 using Swashbuckle.AspNetCore.SwaggerGen;
-
+using Serilog;
 
 namespace CacheCalculator.Controllers
 {
     [Produces("application/json")]
-    [Route("api/Calculator")]
+    [Route("api/v2/Calculator")]
     public class CalculatorController : Controller
     {
+        /// <summary>
+        /// Sum takes an array of integer arguments and return their sum.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         [HttpPost, Route("Sum")]
         [SwaggerResponse((int)HttpStatusCode.OK, typeof(Sum), "Sum of all integers in the input array.")]
         [SwaggerResponse((int)HttpStatusCode.BadRequest, typeof(Error), "Bad or empty request.")]
         public IActionResult Sum([FromBody] int[] args)
         {
-            Sum s; s.v = 42;
+            Log.Debug("hepp");
+            if (null == args)
+                return BadRequest(new Error("No array argument received by server."));
+            Sum s; s.v = 0;
+            foreach (var a in args)
+                s.v += a;
+
             return Ok(s);
         }
 
     }
 
+    /// <summary>
+    /// Jadajada.
+    /// </summary>
     public struct Sum
     {
         public int v;
     }
 
-    public struct Error
+    /// <summary>
+    /// Error description holder.
+    /// </summary>
+    public class Error
     {
+        string what_;
+        public Error(string what)
+        {
+            what_ = what;
+        }
+
+        public string what => what_;
     }
 
 }
--- Src/WebService/CacheCalculator/CacheCalculator/Startup.cs
+++ Src/WebService/CacheCalculator/CacheCalculator/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +9,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
+using Swashbuckle.AspNetCore.Swagger;
+
+using System.IO;
+using System.Reflection;
+using Serilog;
+
 namespace CacheCalculator
 {
     public class Startup
@@ -16,6 +22,13 @@ namespace CacheCalculator
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+
+            Log.Debug("Startup");
+
         }
 
         public IConfiguration Configuration { get; }
@@ -23,6 +36,20 @@ namespace CacheCalculator
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v2", new Info
+                {
+                    Title = "Caching Calculator API",
+                    Version = "v2",
+                    Description = "The Caching Calculator combines calculations with database caching."
+                });
+
+                options.IncludeXmlComments(Path.ChangeExtension(Assembly.GetEntryAssembly().Location, "xml"));
+                options.DescribeAllEnumsAsStrings();
+            });
+
             services.AddMvc();
         }
 
@@ -33,6 +60,13 @@ namespace CacheCalculator
             {
                 app.UseDeveloperExceptionPage();
             }
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "Dualog.Net Api v2");
+            });
 
             app.UseMvc();
         }
