275f5dd237b55ba58e5b93b3d0d0e6b021ada177
Asgeir Finnseth <asgeir@dualog.no>
2017-10-19 16:33:28 +0200

    Added FtvConfigController.cs file.

---
+++ Src/WebService/FTConfig/FTConfig/Controllers/FtvConfigController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Net;
+using Serilog;
+using System.Data.SqlClient;
+
+namespace FTConfig.Controllers
+{
+
+    [Produces("application/json")]
+    [Route("api/v1/FtvConfig")]
+    public class FtvConfigController : Controller
+    {
+        public struct PingArg
+        {
+            public string greetings;
+        }
+        struct PingResponse
+        {
+            public string response;
+        }
+
+        [HttpPost, Route("Ping")]
+        [SwaggerResponse((int)HttpStatusCode.OK, typeof(PingResponse), "Ping to check the connection.")]
+        public IActionResult Ping([FromBody] PingArg arg)
+        {
+            Log.Debug("Ping");
+            PingResponse pr;
+            pr.response = arg.greetings + " to you too."; ;
+            return Ok(pr);
+        }
+
+
+    }
+
+
+}
