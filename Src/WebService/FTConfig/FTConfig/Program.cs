70810062ddb37b948d5c34f91e51a72ebcc20b82
Asgeir Finnseth <asgeir@dualog.no>
2017-10-19 16:31:49 +0200

    First running version with a Ping API entry.

--- Src/WebService/FTConfig/FTConfig/Program.cs
+++ Src/WebService/FTConfig/FTConfig/Program.cs
@@ -7,11 +7,30 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Win32;
+using Serilog;
+using System.Net;
 
 namespace FTConfig
 {
     public class Program
     {
+
+        private static string regKey = @"SOFTWARE\DUALOG\FtConfig";
+
+        public static int GetFromReg(string name, int defaultValue = 0)
+        {
+            var reg = Registry.LocalMachine.OpenSubKey(regKey);
+            if (null == reg)
+            {
+                Log.Debug("Failed to read API port number from Registry Key: " + regKey);
+                return defaultValue;
+            }
+            var ro = (int)reg.GetValue(name, defaultValue);
+            return ro;
+        }
+
+
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
@@ -19,7 +38,21 @@ namespace FTConfig
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>
+                {
+                options.Listen(IPAddress.Any, GetFromReg("PortNo", 98));         // http:*:101
+                    //options.Listen(IPAddress.Any, 443, listenOptions =>
+                    //{
+                    //    listenOptions.UseHttps("certificate.pfx", "password");
+                    //}
+                    //);
+                })
                 .UseStartup<Startup>()
                 .Build();
+
+        //WebHost.CreateDefaultBuilder(args)
+        //    .UseStartup<Startup>()
+        //    .UseUrls("https://*:" + GetFromReg("PortNoPublic", 98))
+        //    .Build();
     }
 }
--- Src/WebService/FTConfig/FTConfig/Startup.cs
+++ Src/WebService/FTConfig/FTConfig/Startup.cs
@@ -8,6 +8,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Serilog;
+using Swashbuckle.AspNetCore.Swagger;
+using System.IO;
+using System.Reflection;
 
 namespace FTConfig
 {
@@ -16,6 +20,12 @@ namespace FTConfig
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+
+            Log.Debug("Startup FtConfig");
         }
 
         public IConfiguration Configuration { get; }
@@ -23,6 +33,19 @@ namespace FTConfig
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info
+                {
+                    Title = "FtConfig API",
+                    Version = "v1",
+                    Description = "The Configuration Entry Point for Dualog File Transfer Config."
+                });
+
+                options.IncludeXmlComments(Path.ChangeExtension(Assembly.GetEntryAssembly().Location, "xml"));
+                options.DescribeAllEnumsAsStrings();
+            });
+
             services.AddMvc();
         }
 
@@ -33,6 +56,12 @@ namespace FTConfig
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dualog File Transfer Config Api v1");
+            });
 
             app.UseMvc();
         }
