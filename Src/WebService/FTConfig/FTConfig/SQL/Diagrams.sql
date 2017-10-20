f65869294d7b83a6e2e9f37237ca61bedcfd2b07
Asgeir Finnseth <asgeir@dualog.no>
2017-10-20 09:30:26 +0200

    Removed Diagrams.sql

--- Src/WebService/FTConfig/FTConfig/SQL/Diagrams.sql
+++
@@ -1,9 +0,0 @@
-
-DROP INDEX "ftdui_transferjob"."ftdui_transferjob_default_source_id";
-DROP INDEX "ftdui_source"."ftdui_source_client_id";
-DROP INDEX "ftdui_source"."ftdui_source_folder_id";
-DROP INDEX "ftdui_source"."ftdui_source_job_id";
-DROP INDEX "ftdui_destination"."ftdui_destination_client_id";
-DROP INDEX "ftdui_destination"."ftdui_destination_folder_id";
-DROP INDEX "ftdui_destination"."ftdui_destination_job_id";
-DROP INDEX "ftdui_client"."ftdui_client_key_id";
